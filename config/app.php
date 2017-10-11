4d14a46fa65c7b1416de2f6319fde8a1375cc176
仁王　義康 <niou@2ndwave.jp>
2017-10-11 16:49:45 +0900

    yarn add datatables.net-bsまで

--- config/app.php
+++ config/app.php
@@ -163,6 +163,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 		Collective\Html\HtmlServiceProvider::class,
+		Barryvdh\Debugbar\ServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -228,6 +229,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+		'Debugbar' => Barryvdh\Debugbar\Facade::class,
 
     ],
 
